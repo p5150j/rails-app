d02df433adae768181050c821ff2ce0c56b15df5
Patrick <patrick.ortell@arus.io>
2021-05-21 13:32:48 -0600

    added first mvc junk

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-root "static_pages#landing_page"
-  #get 'static_pages/privacy_policy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index"
 
-  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  resources :articles
+
+
+
 end
