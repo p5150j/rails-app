4a6ef0388547c39dd30e01656a4f46777577ea55
Patrick <patrick.ortell@arus.io>
2021-05-21 21:45:28 -0600

    added stuff

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  has_one_attached :profile_picture, dependent: :destroy
+  validates :profile_picture, content_type: [:png, :jpg, :jpeg]
 end
